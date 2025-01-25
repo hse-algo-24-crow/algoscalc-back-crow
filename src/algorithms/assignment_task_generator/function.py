2a6c6f4fa5a1a67c920f96cab320e0ac172d639f
Sofa Vlasova <sofa.vlasova2004@gmail.com>
2025-01-25 21:31:02 +0500

    исправления

--- src/algorithms/assignment_task_generator/function.py
+++ src/algorithms/assignment_task_generator/function.py
@@ -1,13 +1,12 @@
 import random
 import copy
-from collections import deque
 from typing import Any
-import sys
-sys.path.append("C:\\Users\\Софья\\algoscalc-back-crow")
-
+# import sys
 from src.internal.errors import AlgorithmTypeError, AlgorithmValueError
 from src.internal.errors.exceptions import AlgorithmRuntimeError
 
+# sys.path.append("C:\\Users\\sofav\\algoscalc-back-crow")
+# print(sys.path)
 COSTS = "costs"
 
 ORDER_TYPE_ERR_MSG = "Порядок матрицы не является целым числом"
@@ -25,11 +24,11 @@ def __validate_params(order: int, min_cost: int, max_cost: int) -> None:
     if order <= 0:
         raise AlgorithmValueError(ORDER_ERR_MSG)
     if not isinstance(min_cost, int):
-        raise AlgorithmTypeError(MIN_COST_TYPE_ERR_MSG )
+        raise AlgorithmTypeError(MIN_COST_TYPE_ERR_MSG)
     if min_cost <= 0:
         raise AlgorithmValueError(MIN_COST_ERR_MSG)
     if not isinstance(max_cost, int):
-        raise AlgorithmTypeError(MAX_COST_TYPE_ERR_MSG )
+        raise AlgorithmTypeError(MAX_COST_TYPE_ERR_MSG)
     if max_cost <= 0:
         raise AlgorithmValueError(MAX_COST_ERR_MSG)
     if min_cost > max_cost:
@@ -47,25 +46,23 @@ def generate_matrix(order: int, min_cost: int, max_cost: int):
     __validate_params(order, min_cost, max_cost)
 
     # создаем матрицу заданного порядка
-    matrix = [[0 for _ in range(order)] for _ in range(order)] 
+    matrix = [[0 for _ in range(order)] for _ in range(order)]
 
     # заполняем матрицу рандомно сгенерированными значениями
-    for l in range(order):
-            for j in range(order):
-                matrix[l][j] = random.randint(min_cost, max_cost)
+    for i in range(order):
+        for j in range(order):
+            matrix[i][j] = random.randint(min_cost, max_cost)
 
     # создаем копию матрицы
-    clone = copy.copy(matrix)
     # до тех пор пока в матрице присутствует совершенное парасочетание перезаполняем ее
-    while not validate_matrix(order, clone):
-            for i in range(order):
-                for j in range(order):
-                    matrix[i][j] = random.randint(min_cost, max_cost)
-            clone = copy.copy(matrix)
+    while  has_perfect_matching(order, matrix):
+        for i in range(order):
+            for j in range(order):
+                matrix[i][j] = random.randint(min_cost, max_cost)
     return matrix
 
 
-def validate_matrix(order: int, matrix: list[list[int]]) -> bool:
+def  has_perfect_matching(order: int, matrix: list[list[int]]) -> bool:
     """
     Проверка матрицы на наличие в ней совершенного парасочетания
     :param order: Порядок матрицы затрат,
@@ -73,51 +70,61 @@ def validate_matrix(order: int, matrix: list[list[int]]) -> bool:
     :return: булевое значение, где True - парасочетание найдено, False - не найдено
     """
 
+    clone = copy.copy(matrix)
     # редукция матрицы по строкам
     for idx in range(order):
-        matrix[idx] = [int(x-min(matrix[idx])) for x in matrix[idx]]
+        clone[idx] = [int(x-min(clone[idx])) for x in clone[idx]]
 
     # редукция матрицы по столбцам
-    minimum = 1000000
+    minimum = float('inf')
     i = 0
     while i != order:
         for j in range(order):
-            minimum = min(matrix[j][i], minimum)
+            minimum = min(clone[j][i], minimum)
         for j in range(order):
-            matrix[j][i]-= minimum
+            clone[j][i] -= minimum
         i += 1
-        minimum = 1000000
-    
-    graph = [[] for _ in range(order)] 
+        minimum = float('inf')
+
+    graph = [[] for _ in range(order)]
 
     # для каждой из работ сохраняем возможных работников
     for i in range(order):
         for j in range(order):
-            if matrix[i][j] == 0:
+            if clone[i][j] == 0:
                 graph[i].append(j)
-    
-    # итоговое назначение работников на работы
-    result = [-1] * order  
 
-    def bpm(u, seen):
-        # Перебираем всех работников, которые могут быть назначены работе 
-        for v in graph[u]:
-            if not seen[v]:  # Если работник еще не был проверен
-                seen[v] = True # Помечаем работника как проверенного
-                # Если работник не назначен ни на одну работу или 
-                # если мы можем переназначить текущую работу работника 
-                if result[v] == -1 or bpm(result[v], seen):
-                    result[v] = u # Назначаем работника на работу 
-                    return True
-        return False
+    # итоговое назначение работников на работы
+    result = [-1] * order
 
     max_matches = 0 # счетчик ребер в максимальном парасочетании
     for u in range(order):
         seen = [False] * order
-        if bpm(u, seen):
+        if bpm(u, seen, graph, result):
             max_matches += 1
 
     return max_matches == order # если число ребер в максимальном парасочетании равно количеству работ => совершенное парасочетание найдено
+
+def bpm(u, seen, graph, result):
+    """
+    Проверяет возможность назначения работников на работы. Поиск в глубину.
+    :param u: работа,
+    :param graph: связи между работниками и работами,
+    :param result: итоговое распределение работников на работы,
+    :param seen: просмотренные работники.
+    :return: булевое значение
+    """
+
+    # Перебираем всех работников, которые могут быть назначены работе
+    for v in graph[u]:
+        if not seen[v]:  # Если работник еще не был проверен
+            seen[v] = True # Помечаем работника как проверенного
+            # Если работник не назначен ни на одну работу или
+            # если мы можем переназначить текущую работу работника
+            if result[v] == -1 or bpm(result[v], seen, graph, result):
+                result[v] = u # Назначаем работника на работу
+                return True
+    return False
 
     
 def main(order: int, min_cost: int, max_cost: int) -> dict[str, Any]:
